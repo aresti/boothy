591d19e61b894838abf6498f31d56d280fb54fc2
joshquick <j.quick@bham.ac.uk>
2016-01-30 10:20:57 +0000

    Add files to coping queue

--- copy_queue.py
+++ copy_queue.py
@@ -1,3 +1,4 @@
+#adapted from http://stackoverflow.com/questions/28305270/how-to-do-asynchronous-file-copying-in-python
 import os
 import sys
 import shutil
@@ -8,31 +9,36 @@ import time
 
 class FileCopy(threading.Thread):
 
-	def __init__(self, queue, files, dir):
+	def __init__(self, watch_dir, volume, queue, exit=False):
 		threading.Thread.__init__(self)
+		self.watch_dir = watch_dir
+		self.volume = volume
 		self.queue = queue
-		self.dir = dir
-		self.files = files
+		self.exit = exit
 
 	def run(self):
-		# Copy files then add them to the queue
-		for f in self.files:
-			exists_path = self.dir + f.split('/')[-1]
-			if os.path.exists(exists_path):
-				i = 1
-				while True:
-					a = exists_path + '(%s)' %i
-					if not os.path.exists(a):
-						shutil.move(f, a)
-						print 'Completed move', a
-						break
-					else:
-						i += 1
+		while True:
+			if self.exit == True:
+				break
+			if self.queue.empty():
+				time.sleep(1)
 			else:
-				shutil.move(f, self.dir)
-				print 'Completed move', f
-			self.queue.put(f)
-		self.queue.put(None)  # signal completion
+				f = self.queue.get()
+				exists_path = self.volume + f.split('/')[-1]
+				if os.path.exists(exists_path):
+					i = 1
+					while True:
+						a = exists_path + '(%s)' %i
+						if not os.path.exists(a):
+							shutil.move(f, a)
+							print 'Completed move', a
+							break
+						else:
+							i += 1
+				else:
+					shutil.move(f, self.volume)
+					print 'Completed move', f
+				self.queue.task_done()
 
 def main():
 	#volume = '/media/UNTITLED/' + 'BOOTHY_STORE'
@@ -40,24 +46,22 @@ def main():
 	volume = '/Users/josh/Downloads/boothy/' + 'test/'	
 	if not os.path.exists(volume):
 		os.makedirs(volume)
+	queue = Queue.Queue()
+	copythread = FileCopy(watch_dir, volume, queue)
+	copythread.start()
 
 	try:
 		while True:
 			files = glob.glob(watch_dir + '*jpg')
 			if files:
-				queue = Queue.Queue()
-				print 'Copying %s %s' %(len(files), ('file' if len(files) == 1 else 'files'))
-				copythread = FileCopy(queue, files, volume)
-				copythread.start()
-				while True:
-					x = queue.get()
-					if x is None:
-						break
-				copythread.join()
-				print 'Completed batch...'
-			time.sleep(1)
+				print 'Queueing %s %s' %(len(files), 
+						       ('file' if len(files) == 1 else 'files'))
+				for f in files:
+					copythread.queue.put(f)
+			time.sleep(2)
 	except KeyboardInterrupt:
-		pass
+		copythread.exit = True
+		copythread.join()
 
 if __name__ == '__main__':
 	main()
