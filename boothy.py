8c87ec16efc7cb21fa711d64591970e02b3a5bdc
Andy Smith <andysmith.d@gmail.com>
2016-01-30 11:25:16 +0000

    started PhotoBooth class

--- boothy.py
+++ boothy.py
@@ -14,6 +14,32 @@ import logging
 
 from pygame.locals import *
 
+class PhotoBooth():
+
+	def __init__(self, gopro_ip, gopro_pwd, photo_mode='still', cntdwn=3, striplength=4):
+		self.gopro_ip
+		self.gopro_pwd
+		self.photo_mode = photo_mode
+		self.cntdwn = cntdwn
+		self.striplength = striplength
+
+	def camera_init(self):
+		self.camera = picamera.PiCamera()
+		self.camera.vflip = True
+
+	def pygame_init(self):
+		pygame.init()
+		self.screen_info = pygame.display.Info()
+		self.w, self.h = self.screen_info.current_w, self.screen_info.current_h
+		self.display_surface = pygame.display.set_mode((w,h),pygame.FULLSCREEN)
+
+	gopro = goprohero.GoProHero(password=settings.gopro['passwd'])
+	if gopro.status()['npics']:
+		time.sleep(1)
+		gopro.command('delete_all')	
+		time.sleep(10)
+	
+
 font_paths = {}
 for f in settings.fonts:
 	font_paths[f] = pygame.font.match_font(settings.fonts[f])
@@ -172,19 +198,6 @@ def main():
 	logger.addHandler(handler)
 	logger.info('Boothy started')
 
-	camera = picamera.PiCamera()
-	camera.vflip = True
-
-	pygame.init()
-	screen_info = pygame.display.Info()
-	w, h = screen_info.current_w, screen_info.current_h
-	screen = pygame.display.set_mode((w,h),pygame.FULLSCREEN)
-
-	gopro = goprohero.GoProHero(password=settings.gopro['passwd'])
-	if gopro.status()['npics']:
-		time.sleep(1)
-		gopro.command('delete_all')	
-		time.sleep(10)
 
 	gopro.command('mode', '%s' %args.mode)
 	time.sleep(1)
